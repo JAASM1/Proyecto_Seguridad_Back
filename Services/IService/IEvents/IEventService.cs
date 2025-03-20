b82d35c7f00847f0407ec01260b48a9afad7bf3e
secondglitter <joshgonzbv@gmail.com>
2025-03-19 20:27:10 -0500

    se creo el get para el token en eventcontroller

--- Services/IService/IEvents/IEventService.cs
+++ Services/IService/IEvents/IEventService.cs
@@ -6,6 +6,7 @@ namespace back_sistema_de_eventos.Services.IService.IEvents
     {
         Task<List<Event>> GetEventsByUser(int idUser);
         Task<Event> GetEventById(int idEvent);
+        Task<Event> GetEventByToken(string Token);
         Task<Event> CreateEvent(Event eventToCreate);
         Task<Event> UpdateEvent(Event eventToUpdate);
         Task<Event> DeleteEvent(int idEvent);
