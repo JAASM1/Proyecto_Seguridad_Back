8e9c6f08a700ca04e7bb63f779a4f6954cab47b9
ACmonse11 <133180292+ACmonse11@users.noreply.github.com>
2025-03-21 21:55:08 -0500

    Eliminacion del crear

--- obj/Debug/net8.0/back_sistema_de_eventos.AssemblyInfo.cs
+++ obj/Debug/net8.0/back_sistema_de_eventos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48fd4f77d2b21b2812b1132af7e072e2e08765a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4ac7f0c112d07da40815b7e84e1184e13d22e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
