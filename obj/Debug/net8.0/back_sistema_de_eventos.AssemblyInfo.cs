8a064ac20d530e8b99318ab6ec959de76208e56f
Eveyas <140725035+Eveyas@users.noreply.github.com>
2025-03-25 11:44:35 -0500

    cambios

--- obj/Debug/net8.0/back_sistema_de_eventos.AssemblyInfo.cs
+++ obj/Debug/net8.0/back_sistema_de_eventos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df85c93c1cc3c8e882fda7f10ce04aef9a5e3a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49be90893802c62387a09fdb49435e0a72d03db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
