498b58a7437d62fd37af2d6ff5977498d8eb6c1d
JAASM1 <alexdroide466@gmail.com>
2025-03-25 12:17:45 -0500

    mm

--- obj/Debug/net8.0/back_sistema_de_eventos.AssemblyInfo.cs
+++ obj/Debug/net8.0/back_sistema_de_eventos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00e3b64e8f6bbf9a07cbf969a2477ea0795bce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c998d0050c8d1e425f3d04d800c3e52506dc937")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_sistema_de_eventos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
